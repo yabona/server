c843dd6c590f2478bff980826a6d9a6421a9381a
unknown <noahbailey9214201@gmail.com>
2017-05-11 13:11:25 -0400

    new file:   Install-DomainControllerQuick.ps1

---
+++ Install-DomainControllerQuick.ps1
@@ -0,0 +1,9 @@
+﻿param (
+    [paramater(mandatory=$true)][String]$domainName
+)
+
+Install-WindowsFeature ad-domain-services -IncludeManagementTools
+
+Install-ADDSForest -CreateDNSDelegation:$false -DomainName $domainName `
+    -DomainMode "Win2012" -ForestMode "Win2012" -InstallDNS:$True  `
+    -SafeModeAdministratorPassword ((Get-Credential).Password) -Force:$true
