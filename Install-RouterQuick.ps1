b0bbf2f2d4ec93b45473e02753ef622d6cbe4fd1
unknown <noahbailey9214201@gmail.com>
2017-06-09 09:42:20 -0400

    new file:   Install-RouterQuick.ps1 	new file:   Mod2.ps1

---
+++ Install-RouterQuick.ps1
@@ -0,0 +1,14 @@
+﻿# ====================================================================== #
+# router configuration
+
+Rename-Computer -newname CiscoTM
+Get-Netadapter Ethernet* | Rename-Netadapter -NewName PROD
+New-NetIpAddress -interfacealias PROD -ipaddress 192.168.100.254 -prefixlength 24 
+Set-DnsClientServerAddress -interfacealias PROD -ServerAddress 192.168.100.10 
+# add second network interface
+Get-Netadapter Ethernet* | Rename-Netadapter -NewName REMOTE
+New-NetIpAddress -interfacealias PROD -ipaddress 192.168.200.254 -prefixlength 24
+#configure routing
+Install-WindowsFeature Routing -IncludeManagementTools
+Get-NetAdapter | Set-NetIpInterface -Forwarding Enabled 
+Restart-Computer 
---
+++ Mod2.ps1
@@ -0,0 +1,30 @@
+﻿# ====================================================================== #
+# Config 
+Get-Netadapter Ethernet* | Rename-Netadapter -NewName PROD
+New-NetIpAddress -interfacealias PROD -ipaddress 192.168.100.10 -prefixlength 24 -defaultGateway 192.168.100.254
+Set-DnsClientServerAddress -interfacealias PROD -ServerAddress 192.168.100.10 
+
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+Rename-Computer -NewName $compName -Restart 
+
+wmic --% useraccount where name="Administrator" rename LocalAdmin
+
+Add-Computer -DomainName ad.elgoog.com -OUPath "ou=infra,dc=fanco,dc=com"  -Restart
+
+# ====================================================================== #
+# DC
+install-windowsfeature ad-domian-services
+Install-ADDSForest -CreateDNSDelegation:$false -DomainName "fanco.com" `
+    -DomainMode "Win2012" -ForestMode "Win2012" -InstallDNS:$True `
+    -SafeModeAdministratorPassword ((Get-Credential).Password) -Force:$true
+
+# ====================================================================== #
+# router configuration
+
+Install-WindowsFeature Routing -IncludeManagementTools
+Get-NetAdapter | Set-NetIpInterface -Forwarding Enabled 
+
+# ====================================================================== #
+Install-WindowsFeature fs-iscsitarget-server -IncludeManagementTools
+
