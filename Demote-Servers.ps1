752c4d7f70780a5d923a79a1626d7eefd4a6c6c9
unknown <noahbailey9214201@gmail.com>
2017-07-14 11:56:34 -0400

    new file:   Demote-Servers.ps1

---
+++ Demote-Servers.ps1
@@ -0,0 +1,9 @@
+﻿$LAcred = "Windows1" | ConvertTo-SecureString -AsPlainText -Force
+
+Uninstall-ADDSDomainController -LocalAdministratorPassword:$LAcred -NoRebootOnCompletion 
+
+Restart-Computer -force
+
+Remove-Computer -UnjoinDomainCredential $DAcred -LocalCredential $LAcred -Restart
+
+netdom resetpwd /server:$pdc /userd:$user /passwordd:* 
