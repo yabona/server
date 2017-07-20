23af81e841cb47fd03983f8254cf7989f160759c
yabona <noahbailey9214201@gmail.com>
2017-07-19 21:34:09 -0400

    Update JustFkingUpdate.ps1

--- JustFkingUpdate.ps1
+++ JustFkingUpdate.ps1
@@ -1,18 +1,33 @@
 # how to insta-DDOS your domain controllers!
 # Please never do this IRL!
 
-# especially this. This is bad. 
+# especially this: 
 $Computers = (Get-adcomputer -filter *).name
+# it's so wrong and so right
+$domainControllers = $Computers.Where({$_ -like "*DC*"})
 
+# Replicate and sync all DCs in forest:  
+foreach ($i in $domainControllers) {
+    repadmin /kcc $i
+    repadmin /syncall $i /APed
+}
+
+# Update all GPs. 
 foreach ($i in $Computers) {
+    # test connection first...
     if (Test-NetConnection $i -ErrorAction SilentlyContinue) {
+        
+        # update GP immediately on all responsive machines: 
         Write-Verbose "Updating policy on $i`:" -Verbose
-        Invoke-GPUpdate -Computer $i -force -target Computer -Verbose -AsJob
+        Invoke-GPUpdate -Computer $i -Force -RandomDelayInMinutes 0 -Verbose -AsJob
     }
 }
 
 # wait for jobs to complete
-Get-Job | Wait-Job | Remove-Job
-# this feels wrong but so right
+Get-Job | Wait-Job 
+Get-job | Remove-Job
 
+# m-m-m-MONEYSHOT
+repadmin /replsummary 
 Write-verbose "Finished refreshing GP settings!" -Verbose
+Write-Verbose "Finished replicating all DCs! " -Verbose
