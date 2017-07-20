06f8bf7d44e15f8223e2fb3e3db08aee9ede0edc
yabona <noahbailey9214201@gmail.com>
2017-07-19 21:50:52 -0400

    Update JustFkingUpdate.ps1

--- JustFkingUpdate.ps1
+++ JustFkingUpdate.ps1
@@ -1,33 +1,25 @@
 # how to insta-DDOS your domain controllers!
 # Please never do this IRL!
 
-# especially this: 
-$Computers = (Get-adcomputer -filter *).name
-# it's so wrong and so right
-$domainControllers = $Computers.Where({$_ -like "*DC*"})
-
-# Replicate and sync all DCs in forest:  
-foreach ($i in $domainControllers) {
-    repadmin /kcc $i
-    repadmin /syncall $i /APed
-}
-
-# Update all GPs. 
-foreach ($i in $Computers) {
-    # test connection first...
-    if (Test-NetConnection $i -ErrorAction SilentlyContinue) {
-        
-        # update GP immediately on all responsive machines: 
-        Write-Verbose "Updating policy on $i`:" -Verbose
-        Invoke-GPUpdate -Computer $i -Force -RandomDelayInMinutes 0 -Verbose -AsJob
+foreach ($i in (Get-ADForest).Domains) {
+    (Get-ADComputer -server $i -filter {Name -like "*DC*" -and Enabled -eq $True } ).Name | % {
+        Write-Verbose $_ -Verbose
+        repadmin /kcc $_
+        repadmin /syncall $_ /APed
     }
 }
 
-# wait for jobs to complete
+foreach ($i in (Get-ADForest).Domains) {
+    foreach ($j in (Get-ADComputer -server $i -filter {Enabled -eq $true} ).dnsHostName ) {
+        if (Test-NetConnection $j -ErrorAction SilentlyContinue) {
+            Write-Verbose "Updating policy on $j`:" -Verbose
+            Invoke-GPUpdate -Computer $j -Force -RandomDelayInMinutes 0 -Verbose -AsJob
+        }
+    }
+}
+
 Get-Job | Wait-Job 
 Get-job | Remove-Job
-
-# m-m-m-MONEYSHOT
 repadmin /replsummary 
 Write-verbose "Finished refreshing GP settings!" -Verbose
 Write-Verbose "Finished replicating all DCs! " -Verbose
