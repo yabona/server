9a0b85d7c260f8e12ac7c963b70301b70a535ad4
n <n@win32cih.localdomain>
2018-01-20 08:33:10 -0500

    modified:   FastDhcpServer.ps1

--- FastDhcpServer.ps1
+++ FastDhcpServer.ps1
@@ -1,22 +1,35 @@
 ﻿# Give me DHCP dammit!
+# Run as DA, fuck best practices! 
 
-Install-WindowsFeature dhcp -IncludeManagementTools
+param(
+  [Paramater(Mandatory=$True)]$TargetDomainController,
+)
 
-$DC_IPconfig = Get-NetIPConfiguration
+$RemoteCommandInvocation = {
 
-[System.Collections.ArrayList]$addr = $DC_IPconfig.ipv4address.ipaddress.split('.')
-[string]$subnet = "$([string]$addr[0]).$([string]$addr[1]).$([string]$addr[2])"
+  Install-WindowsFeature dhcp -IncludeManagementTools
 
-if($DC_IPconfig.ipaddress.prefixlength -ne 24) {
-    $subnetMask = Read-Host "subnet mask"
-}else{ $subnet = $DC_IPconfig.ipaddress.prefixlength }
+  $DC_IPconfig = Get-NetIPConfiguration
 
-Add-DhcpServerv4Scope -StartRange "$subnet.200" -EndRange "$subnet.250"  `
-    -name DHCP_Server -SubnetMask $SubnetMask
+  [System.Collections.ArrayList]$addr = $DC_IPconfig.ipv4address.ipaddress.split('.')
+  [string]$subnet = "$([string]$addr[0]).$([string]$addr[1]).$([string]$addr[2])"
+      # that's shit and I feel ashamed
 
-Set-DhcpServerv4OptionValue -ScopeId "$subnet.0" `
-    -DnsDomain $DC_IPconfig.netprofile.name `
-    -DnsServer $DC_IPconfig.ipv4address.ipaddress `
-    -Router $DC_IPconfig.IPv4defaultGateway.Nexthop
+  if($DC_IPconfig.ipaddress.prefixlength -ne 24) {
+      $subnetMask = Read-Host "subnet mask"
+  }else{ $subnet = $DC_IPconfig.ipaddress.prefixlength }
 
-Add-DhcpServerInDC -DnsName $(hostname)
+  Add-DhcpServerv4Scope -StartRange "$subnet.200" -EndRange "$subnet.250"  `
+      -name DHCP_Server -SubnetMask $SubnetMask
+
+  Set-DhcpServerv4OptionValue -ScopeId "$subnet.0" `
+      -DnsDomain $DC_IPconfig.netprofile.name `
+      -DnsServer $DC_IPconfig.ipv4address.ipaddress `
+      -Router $DC_IPconfig.IPv4defaultGateway.Nexthop
+
+  Add-DhcpServerInDC -DnsName $(hostname)
+
+}
+
+# .. .. .. .. .. .. ..
+Invoke-Command -computername:$TargetDomainController -Scriptblock:$RemoteCommandInvocation
